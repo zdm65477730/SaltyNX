420618141e27caf1ba69a4070c972df1e36f4838
masagrator <masagrator@gmail.com>
2020-12-17 11:53:38 +0100

    Update main.c

--- saltysd_core/source/main.c
+++ saltysd_core/source/main.c
@@ -65,6 +65,8 @@ void __libnx_init(void* ctx, Handle main_thread, void* saved_lr)
 
 void __attribute__((weak)) __libnx_exit(int rc)
 {
+	fsdevUnmountAll();
+
 	// Call destructors.
 	void __libc_fini_array(void);
 	__libc_fini_array();
@@ -301,7 +303,7 @@ Result svcSetHeapSizeIntercept(u64 *out, u64 size)
 	
 	if (!ret)
 	{
-		*out += 0x200000;
+		*out += ((elf_area_size+0x200000) & 0xffe00000));
 	}
 	
 	return ret;
