efd9b061f786cd2c352349d5ba11e53cddb7a5d6
MasaGratoR <masagrator@gmail.com>
2022-05-01 16:16:40 +0200

    Add files via upload

--- libnx_min/nx/include/switch_min/kernel/virtmem.h
+++ libnx_min/nx/include/switch_min/kernel/virtmem.h
@@ -7,6 +7,26 @@
 #pragma once
 #include "../types.h"
 
+/// Address space reservation type (see \ref virtmemAddReservation)
+typedef struct VirtmemReservation VirtmemReservation;
+
+void virtmemSetup(void);
+
+/// Locks the virtual memory manager mutex.
+void virtmemLock(void);
+
+/// Unlocks the virtual memory manager mutex.
+void virtmemUnlock(void);
+
+/**
+ * @brief Finds a random slice of free general purpose address space.
+ * @param size Desired size of the slice (rounded up to page alignment).
+ * @param guard_size Desired size of the unmapped guard areas surrounding the slice  (rounded up to page alignment).
+ * @return Pointer to the slice of address space, or NULL on failure.
+ * @note The virtual memory manager mutex must be held during the find-and-map process (see \ref virtmemLock and \ref virtmemUnlock).
+ */
+void* virtmemFindAslr(size_t size, size_t guard_size);
+
 /**
  * @brief Reserves a slice of general purpose address space.
  * @param size The size of the slice of address space that will be reserved (rounded up to page alignment).
